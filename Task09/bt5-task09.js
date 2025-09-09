9ac6c02c98e0170ab6f870a0c0b2bd00a2ba258c
Vinh-tech05 <vinhpcph50853@gmail.com>
2025-09-09 22:00:08 +0700

    Task09

---
+++ Task09/bt5-task09.js
@@ -0,0 +1,22 @@
+Array.prototype.forEach2 = function (callback) {
+  const length = this.length;
+  for (let i = 0; i < length; i++) {
+    if (i in this) {
+      callback(this[i], i, this);
+    }
+  }
+};
+
+// Sample usage
+const arr = [1, 2, 3, 4, 5];
+
+arr.forEach2((value, index) => {
+  console.log(`Value at index ${index}: ${value}`);
+});
+
+// Output:
+// Value at index 0: 1
+// Value at index 1: 2
+// Value at index 2: 3
+// Value at index 3: 4
+// Value at index 4: 5
