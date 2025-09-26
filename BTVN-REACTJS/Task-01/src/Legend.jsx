4e40c368b69f948596b64c8a43511f9fb6b4f64a
Vinh-tech05 <vinhpcph50853@gmail.com>
2025-09-26 20:51:38 +0700

    Task-01

--- BTVN-REACTJS/Task-01/src/Legend.jsx
+++ BTVN-REACTJS/Task-01/src/Legend.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Legend = () => {
   const legends = [
-    { label: "Yếu", color: "red" },
-    { label: "Trung Bình", color: "orange" },
-    { label: "Khá", color: "blue" },
-    { label: "Giỏi", color: "green" },
-    { label: "Xuất Sắc", color: "blueviolet" },
+    { label: "Yếu: Nhỏ hơn 3.", color: "red" },
+    { label: "Trung Bình: Trên 3 điểm và nhỏ hơn 6.", color: "orange" },
+    { label: "Khá: Trên 6 điểm và nhỏ hơn hoặc bằng 8.", color: "blue" },
+    { label: "Giỏi: Trên 8 điểm và nhỏ hơn 9.5.", color: "green" },
+    {
+      label: "Xuất Sắc: Trên hoặc bằng 9,5 điểm và nhỏ hơn 10.",
+      color: "blueviolet",
+    },
   ];
   return (
     <div>
