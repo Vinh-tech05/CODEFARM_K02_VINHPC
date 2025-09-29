f56b1a78c60d6983a33bd4b5203380a97c7cd179
Vinh-tech05 <vinhpcph50853@gmail.com>
2025-09-29 21:38:20 +0700

    Task-02

---
+++ BTVN-REACTJS/Task-02/react-swc/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import GuessingGame from "./GuessingGame";
+
+function App() {
+  return (
+    <>
+      <div>
+        <GuessingGame />
+      </div>
+    </>
+  );
+}
+
+export default App;
