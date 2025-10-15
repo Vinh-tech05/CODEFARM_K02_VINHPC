6fbabc7c54a1a8823ecd470591e7ad2996294697
Vinh-tech05 <vinhpcph50853@gmail.com>
2025-10-15 13:31:43 +0700

    task18.1

--- todo-list/src/routes/clientRoutes.jsx
+++ todo-list/src/routes/clientRoutes.jsx
@@ -3,13 +3,14 @@ import LayoutClient from "../layouts/LayoutClient";
 import ImportantPage from "../pages/client/ImportantPage";
 import TodoPage from "../pages/client/TodoPage";
 import TodoDetailPage from "../pages/client/TodoDetailPage";
+import { Navigate } from "react-router-dom";
 
 const clientRoutes = [
   {
     path: "/",
     Component: LayoutClient,
     children: [
-      { index: true, Component: TodoPage },
+      { index: true, element: <Navigate to={"/todos"} /> },
       { path: "todos", Component: TodoPage },
       // Dynamic route
       { path: "todos/:id", Component: TodoDetailPage },
