c6049b622d450b21e638856343d422905fdda354
Vinh-tech05 <vinhpcph50853@gmail.com>
2025-10-21 15:21:20 +0700

    Fix đăng nhập

--- todo-list/src/routes/clientRoutes.jsx
+++ todo-list/src/routes/clientRoutes.jsx
@@ -4,13 +4,18 @@ import ImportantPage from "../pages/client/ImportantPage";
 import TodoPage from "../pages/client/TodoPage";
 import TodoDetailPage from "../pages/client/TodoDetailPage";
 import { Navigate } from "react-router-dom";
+import PrivateRoute from "./protectedRoutes/PrivateRoute";
 
 const clientRoutes = [
   {
     path: "/",
-    Component: LayoutClient,
+    element: (
+      <PrivateRoute>
+        <LayoutClient />
+      </PrivateRoute>
+    ),
     children: [
-      { index: true, element: <Navigate to={"/auth/login"} /> },
+      { index: true, element: <Navigate to={"/todos"} /> },
       { path: "todos", Component: TodoPage },
       // Dynamic route
       { path: "todos/:id", Component: TodoDetailPage },
